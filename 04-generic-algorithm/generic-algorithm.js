76dfe6d7a9773895816d2653d2929a687626912b
Yosuke Saito <yosuke.saito1202@gmail.com>
2016-09-10 16:14:52 +0900

    implement generic algorithm

--- 04-generic-algorithm/generic-algorithm.js
+++ 04-generic-algorithm/generic-algorithm.js
@@ -1,26 +1,117 @@
 'use strict';
 
 let fs = require('fs'),
-    random = require('../lib/random.js');
+    random = require('../lib/random.js'),
+    test1 = { file: 'data.txt',  limit: 1000, pool: 10 },
+    test2 = { file: 'data2.txt', limit:  300, pool: 50 },
+    test = test2; // to change test data
 
-const POOLSIZE = 10;
+const POOLSIZE = test.pool;
 const LAST = 50;
 const MRATE = 0.01;
 
-fs.readFile('data.txt', (err, data) => {
+fs.readFile(test.file, (err, data) => {
     let parcels = initParcels(data),
         n = parcels.length,
-        weightLimit = 1000,
+        weightLimit = test.limit,
         pool = initPool(n),
         newGenPool = initNewGen(n),
         generation;
 
     for (generation = 0; generation < LAST; generation++) {
-        console.log('%d generation', generation);
-        selectNewGen(newGenPool);
+        console.log('%d generation', (generation + 1));
+        mating(pool, newGenPool, n);
+        mutation(newGenPool, n);
+        selectNewGen(newGenPool, n);
+        printPool(pool);
     }
 
-    function selectNewGen(newGenPool) {
+    function printPool(pool) {
+        let i,
+            j,
+            fitness,
+            totalFitness = 0,
+            elite,
+            bestFit = 0,
+            str;
+
+        for (i = 0; i < POOLSIZE; i++) {
+            str = '';
+            for (j = 0; j < n; j++)
+                str += pool[i][j];
+            fitness = evalFit(pool[i]);
+            console.log(str + '\t' + fitness);
+            if (fitness > bestFit) {
+                bestFit = fitness;
+                elite = i;
+            }
+            totalFitness += fitness;
+        }
+        str = '' + elite + '\t' + bestFit + ' \t';
+        console.log(str + (totalFitness / POOLSIZE) + '\n');
+        // console.log(totalFitness / POOLSIZE);
+     }
+
+    function mutation(newGenPool, n) {
+        let i, j;
+
+        for (i = 0; i < POOLSIZE * 2; i++) {
+            for (j = 0; j < n; j++) {
+                if (random.get() <= MRATE)
+                    newGenPool[i][j] = 1 - newGenPool[i][j];
+            }
+        }
+    }
+
+    function crossing(mother, father, chrom1, chrom2, n) {
+        let j, cp;
+
+        cp = random.get(0, n, false);
+
+        for (j = 0; j < cp; j++) {
+            chrom1[j] = mother[j];
+            chrom2[j] = father[j];
+        }
+        for (; j < n; j++) {
+            chrom2[j] = mother[j];
+            chrom1[j] = father[j];
+        }
+    }
+
+    function selectPool(roulette, totalFitness) {
+        let i, ball, acc = 0;
+
+        ball = random.get(0, totalFitness, false);
+        for (i = 0; i < POOLSIZE; i++) {
+            acc += roulette[i];
+            if (acc > ball) break;
+        }
+        return i;
+    }
+
+    function mating(pool, newGenPool, n) {
+        let i,
+            mother,
+            father,
+            totalFitness = 0,
+            roulette = [];
+
+        for (i = 0; i < POOLSIZE; i++) {
+            roulette[i] = evalFit(pool[i]);
+            totalFitness += roulette[i];
+        }
+
+        for (i = 0; i < POOLSIZE; i++) {
+            do {
+                mother = selectPool(roulette, totalFitness);
+                father = selectPool(roulette, totalFitness);
+            } while (mother == father);
+
+            crossing(pool[mother], pool[father], newGenPool[i * 2], newGenPool[i * 2 + 1], n);
+        }
+    }
+
+    function selectNewGen(newGenPool, n) {
         let i, j, c, ball,
             totalFitness,
             roulette = [],
@@ -30,6 +121,17 @@ fs.readFile('data.txt', (err, data) => {
             totalFitness = 0;
             for (c = 0; c < POOLSIZE * 2; c++) {
                 roulette[c] = evalFit(newGenPool[c]);
+                totalFitness += roulette[c];
+            }
+            ball = random.get(0, totalFitness, false);
+            acc = 0;
+            for (c = 0; c < POOLSIZE * 2; c++) {
+                acc += roulette[c];
+                if (acc > ball) break;
+            }
+
+            for (j = 0; j < n; j++) {
+                pool[i][j] = newGenPool[i][j];
             }
         }
     }
