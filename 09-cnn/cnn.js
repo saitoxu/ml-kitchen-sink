0e12d7fe5f062575fd31fb09ebb84946d5651a06
Yosuke Saito <yosuke.saito1202@gmail.com>
2016-09-15 21:36:23 +0900

    update cnn

--- 09-cnn/cnn.js
+++ 09-cnn/cnn.js
@@ -4,13 +4,35 @@ let fs = require('fs'),
     random = require('../lib/random.js');
 
 const INPUTSIZE = 11;
+const FILTERNO = 2;
 const FILTERSIZE = 3;
+const POOLSIZE = 3;
+const POOLOUTSIZE = 3;
+const LIMIT = 0.001;
+const INITIALERR = 100;
+const HIDDENNO = 3;
+const ALPHA = 10;
 
 fs.readFile('data.txt', (err, data) => {
     let input = decode(data.toString()),
-        filters = [ new Filter(), new Filter() ];
+        filters = [],
+        i, j,
+        fc = new FCLayer(),
+        error = INITIALERR;
 
-    console.log(filters);
+    for (i = 0; i < FILTERNO; i++) {
+        filters.push(new Filter());
+    }
+
+    while (error > LIMIT) {
+        error = 0.0;
+        for (i = 0; i < input.length; i++) {
+            for (j = 0; j < FILTERNO; j++) {
+                let convOut = filters[j].conv(input[i].getImage());
+            }
+            // TODO
+        }
+    }
 
     function decode(data) {
         let input = [],
@@ -25,7 +47,7 @@ fs.readFile('data.txt', (err, data) => {
                 indata.setImage(image);
                 input.push(indata);
             } else {
-                // TODO
+                image.push(lines[i].split(' '));
             }
         }
         return input;
@@ -47,6 +69,22 @@ class Filter {
 
     get() {
         return this.filter;
+    }
+
+    conv(image) {
+        let i, j, row,
+            convOut = [],
+            start = Math.floor(FILTERSIZE / 2);
+        for (i = start; i < INPUTSIZE - start; i++) {
+            convOut.push(row = []);
+            for (j = start; j < INPUTSIZE - start; j++) {
+                row.push(this.calcConv(image, i, j));
+            }
+        }
+    }
+
+    calcConv(image, i, j) {
+        return 0; // TODO
     }
 }
 
@@ -70,5 +108,30 @@ class InputData {
 
     setImage(image) {
         this.image = image;
+    }
+}
+
+class FCLayer { // fully connected layer
+    constructor() {
+        let i, j, ary;
+        this.interLayer = [];
+        this.outputLayer = [];
+        for (i = 0; i < HIDDENNO; i++) {
+            this.interLayer.push(ary = []);
+            for (j = 0; j < POOLOUTSIZE * POOLOUTSIZE * FILTERNO + 1; j++) {
+                ary.push(random.get(-1, 1, false));
+            }
+        }
+        for (i = 0; i < HIDDENNO; i++) {
+            this.outputLayer.push(random.get(-1, 1, false));
+        }
+    }
+
+    getInterLayer() {
+        return this.interLayer;
+    }
+
+    getOutputLayer() {
+        return this.outputLayer;
     }
 }
