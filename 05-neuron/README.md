0b6e1bef9c94d2af370884d430b4e1c81026654f
Yosuke Saito <yosuke.saito1202@gmail.com>
2016-10-13 23:51:59 +0900

    refactor neuron program

---
+++ 05-neuron/README.md
@@ -0,0 +1,6 @@
+Neuron
+---
+
+## Description
+
+## Usage
--- 05-neuron/neuron.js
+++ 05-neuron/neuron.js
@@ -1,33 +1,41 @@
+/**
+ * Neuron
+ *
+ * And operator
+ */
 'use strict';
 
-let fs = require('fs'),
-    Neuron = require('../lib/neuron.js');
+const fs = require('fs');
+const Neuron = require('../lib/neuron.js');
 
 fs.readFile('data.txt', (err, data) => {
-    let input = initInput(data),
-        w = [ 1, 1 ],
-        v = 1.5,
-        i;
+  const input = initInput(data);
+  const w = [ 1, 1 ];
+  const v = 1.5;
+  const neuron = new Neuron(w, v, f);
 
-    let neuron = new Neuron(w, v, f);
+  for (let i = 0; i < input.length; i++) {
+    console.log(input[i], neuron.forward(input[i]));
+  }
 
-    for (i = 0; i < input.length; i++) {
-        console.log(input[i], neuron.forward(input[i]));
+  /**
+   * Transfer function
+   *
+   * @param  {Number} u
+   * @return {Number}
+   */
+  function f(u) {
+    return u >= 0 ? 1 : 0;
+  }
+
+  function initInput(data) {
+    const lines = data.toString().split('\n');
+    let input = [];
+
+    for (let i = 0; i < lines.length - 1; i++) {
+      const inputs = lines[i].split(' ');
+      input.push([ parseInt(inputs[0]), parseInt(inputs[1]) ]);
     }
-
-
-    function f(u) {
-        return u >= 0 ? 1 : 0;
-    }
-
-    function initInput(data) {
-        let lines = data.toString().split('\n'),
-            input = [], inputs, i;
-
-        for (i = 0; i < lines.length - 1; i++) {
-            inputs = lines[i].split(' ');
-            input.push([ parseInt(inputs[0]), parseInt(inputs[1]) ]);
-        }
-        return input;
-    }
+    return input;
+  }
 });
